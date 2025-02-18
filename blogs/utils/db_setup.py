ffd0c73d41b26d38a344211a2b50a7eb4b3f236c
Rahul <rahulchoudhary@Rahuls-Laptop.local>
2025-02-18 21:41:37 +0530

    Docker compose filw written

--- blogs/utils/db_setup.py
+++ blogs/utils/db_setup.py
@@ -3,9 +3,9 @@ import os
 
 def get_conn_str():
   return f"""
-  dbname={os.environ.get('POSTGRES_DB',None)}
-  user={os.environ.get('POSTGRES_USER',None)}
-  password={os.environ.get('POSTGRES_PASSWORD',None)}
-  host={os.environ.get('POSTGRES_HOST',None)}
+  dbname={os.environ.get('POSTGRES_DB',"postgres")}
+  user={os.environ.get('POSTGRES_USER',"postgres")}
+  password={os.environ.get('POSTGRES_PASSWORD',"Mysql")}
+  host={os.environ.get('POSTGRES_HOST',"localhost")}
   port={os.environ.get('POSTGRES_PORT',5432)}
   """
